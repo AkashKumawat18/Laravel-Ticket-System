38161ad1a3b80c5d2e2f1de6ccf145115da86bc3
Akash Kumawat <kashu7102@gmail.com>
2024-03-28 21:05:28 +0530

    Help Ticket Project resource route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 use   App\Models\User;
 use \Illuminate\Support\Facades\Auth;
+use \App\Http\Controllers\TicketController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -39,4 +40,8 @@ Route::get('/auth/callback', function () {
     ]);
     Auth::login($user);
     return redirect('/dashboard');
+});
+
+Route::middleware('auth')->prefix('ticket')->group(function(){
+    Route::resource('/',TicketController::class);
 });
